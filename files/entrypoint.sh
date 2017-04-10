30e149f2f8dbef9bbec684d36df5a975396c0f59
Sven van de Scheur <svenvandescheur@gmail.com>
2017-04-10 20:33:20 +0200

    Update entrypoint.sh

--- files/entrypoint.sh
+++ files/entrypoint.sh
@@ -17,6 +17,14 @@ set -o pipefail
 # Create log directory
 mkdir -p /var/log/nginx/
 
+# Lets encryppt
+apt-get -qy update
+apt-get -qy install software-properties-common python-software-properties
+add-apt-repository ppa:certbot/certbot -y
+apt-get -qy update
+apt-get -qy install certbot
+apt-get -qy install python-certbot-nginx
+certbot certonly --agree-tos --keep --noninteractive --standalone -d svenv.nl -d www.svenv.nl -d `hostname` -m svenvandescheur@gmail.com
 
 # Start services
 service rsyslog start
